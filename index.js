c6e50da52fa0b36b9ca182b52e360cfa81ae5469
Fermoj <fer.mojaverian@gmail.com>
2023-01-10 12:48:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+  //skapar app med view
+  data() {
+    //hämtar ut datan från html
+    return {
+      //returnerar data in i html
+      title: "HEJ ALLA",
+    };
+  },
+}).mount("#app"); //exporterar till källan "app" skrivs som css
